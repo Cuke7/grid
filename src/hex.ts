f47ffb6e8b060c81a3fe274e1a62455059ef5c78
Cuke7 <cuke.dev@mail.com>
2023-10-24 17:12:05 +0200

    Adding more utility fucntions

--- src/hex.ts
+++ src/hex.ts
@@ -4,53 +4,81 @@ export type Cell = {
   radius: number;
   index: number;
   content: any;
-  state: "NULL" | "HOVERED" | "ACTIVE";
+  state: "NULL" | "HOVERED" | "ACTIVE" | "GREEN";
 };
 
-export class HexGrid {
-  dimensions;
-  drawHexagon = (_cell: Cell) => {};
-  cells: Cell[] = [];
-  cellRadius: number;
+export type State = {
+  cells: Cell[];
+};
 
-  constructor(
-    dimensions: { rows: number; cols: number },
-    celLRadius: number,
-    drawHexagon: (cell: Cell) => void
-  ) {
-    this.dimensions = dimensions;
-    this.cellRadius = celLRadius;
-    this.drawHexagon = drawHexagon;
-    let index = 0;
-    for (let row = 0; row < dimensions.rows; row++) {
-      for (let col = 0; col < dimensions.cols; col++) {
-        let x = col * ((celLRadius * 3) / 2);
-        let y =
-          row * (celLRadius * Math.sqrt(3)) +
-          ((col % 2) * (celLRadius * Math.sqrt(3))) / 2;
-        this.cells.push({
-          x: x + this.cellRadius,
-          y: y + this.cellRadius,
-          radius: celLRadius,
-          index,
-          content: null,
-          state: "NULL",
-        });
-        index++;
-      }
+export function initState(
+  dimensions: { rows: number; cols: number },
+  celLRadius: number
+): State {
+  let index = 0;
+  let cells: Cell[] = [];
+  for (let row = 0; row < dimensions.rows; row++) {
+    for (let col = 0; col < dimensions.cols; col++) {
+      let x = col * ((celLRadius * 3) / 2);
+      let y =
+        row * (celLRadius * Math.sqrt(3)) +
+        ((col % 2) * (celLRadius * Math.sqrt(3))) / 2;
+      cells.push({
+        x: x + celLRadius,
+        y: y + celLRadius,
+        radius: celLRadius,
+        index,
+        content: null,
+        state: "NULL",
+      });
+      index++;
     }
+  }
+  return { cells };
+}
+
+export class HexGrid {
+  drawHexagon = (_cell: Cell) => {};
+  state: State;
+
+  constructor(state: State, drawHexagon: (cell: Cell) => void) {
+    (this.state = state), (this.drawHexagon = drawHexagon);
   }
 
   drawGrid() {
-    for (const cell of this.cells) {
+    for (const cell of this.state.cells) {
       this.drawHexagon(cell);
+    }
+  }
+
+  getCellsBetweenCells(cell1: Cell, cell2: Cell) {
+    cell1.state = "ACTIVE";
+    cell2.state = "ACTIVE";
+
+    let closestCell = cell1;
+    const result: Cell[] = [];
+
+    while (closestCell.index != cell2.index) {
+      const adjacentCells = this.getAdjacentCells(closestCell);
+      let bestDist = Infinity;
+      adjacentCells.forEach((cell) => {
+        const currentDist = dist(cell2, cell);
+        if (currentDist < bestDist) {
+          bestDist = currentDist;
+          closestCell = cell;
+        }
+      });
+      if (closestCell.index != cell2.index) {
+        result.push(closestCell);
+        closestCell.state = "GREEN";
+      }
     }
   }
 
   // Returns an array containg the adjacent cells
   getAdjacentCells(cell: Cell) {
     const adjacentCells: Cell[] = [];
-    for (const otherCell of this.cells) {
+    for (const otherCell of this.state.cells) {
       if (cell.index != otherCell.index) {
         if (dist(cell, otherCell) < 2 * cell.radius) {
           adjacentCells.push(otherCell);
@@ -61,17 +89,17 @@ export class HexGrid {
   }
 
   getHoveredCell(mouseX: number, mouseY: number) {
-    for (const cell of this.cells) {
+    for (const cell of this.state.cells) {
       let distance = Math.sqrt((cell.x - mouseX) ** 2 + (cell.y - mouseY) ** 2);
-      if (distance < 0.8 * cell.radius) {
+      if (distance < cell.radius) {
         return cell;
       }
     }
   }
 
   resetCellState() {
-    for (const cell of this.cells) {
-      if (cell.state == "HOVERED") cell.state = "NULL";
+    for (const cell of this.state.cells) {
+      cell.state = "NULL";
     }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import P5 from "p5";
-import { HexGrid, Cell } from "./hex";
+import { HexGrid, Cell, initState } from "./hex";
 
 let p: P5;
 
+const state = initState({ rows: 11, cols: 26 }, 20);
+
 // Init grid with drawHexagon callback function
-const grid = new HexGrid({ rows: 11, cols: 26 }, 20, (cell: Cell) => {
+const grid = new HexGrid(state, (cell: Cell) => {
   p.fill("black");
   p.stroke("white");
-  if (cell.state == "HOVERED") {
-    p.fill("gray");
-  }
-  if (cell.state == "ACTIVE") {
-    p.fill("red");
-  }
+  if (cell.state == "HOVERED") p.fill("gray");
+  if (cell.state == "ACTIVE") p.fill("red");
+  if (cell.state == "GREEN") p.fill("green");
+
   p.beginShape();
   for (let i = 0; i < 6; i++) {
     let angle = (p.TWO_PI / 6) * i;
@@ -26,18 +26,23 @@ const grid = new HexGrid({ rows: 11, cols: 26 }, 20, (cell: Cell) => {
 // Creating the sketch itself
 const sketch = (p5: P5) => {
   p = p5;
+
   p5.setup = () => {
     const canvas = p5.createCanvas(800, 400);
     canvas.parent("app");
+    p5.frameRate(24);
   };
 
   // The sketch draw method
   p5.draw = () => {
     p5.background("black");
-    grid.drawGrid();
     grid.resetCellState();
-    const cell = grid.getHoveredCell(p5.mouseX, p5.mouseY);
-    if (cell && cell.state == "NULL") cell.state = "HOVERED";
+    let cell1 = grid.state.cells[0];
+    let cell2 = grid.getHoveredCell(p5.mouseX, p5.mouseY);
+    if (cell2) {
+      grid.getCellsBetweenCells(cell1, cell2);
+    }
+    grid.drawGrid();
   };
 
   p5.mouseClicked = () => {
